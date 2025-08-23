d469ef62c101774a72f2c3837fcd0593ff9a22ab
xLucyu <xlucyyu@gmail.com>
2025-08-23 10:03:39 +0200

    imported images, updated inputfields and paragonselect, added tailwindcss

---
+++ src/assets/paragons/images/index.ts
@@ -0,0 +1,25 @@
+import dartImage from "./ApexPlasmaMaster.webp";
+import ninjaImage from "./AscendedShadow.webp";
+import tackImage from "./CycloneOfFireAndMetal.webp";
+import boomerImage from "./GlaiveDominus.webp";
+import aceImage from "./GoliathDoomship.webp";
+import wizardImage from "./MagusPerfectus.webp";
+import engiImage from "./MasterBuilder.webp";
+import spikeImage from "./MegaMassiveMunitionsFactory.webp";
+import subImage from "./NauticSiegeCore.webp";
+import boatImage from "./NavarchOfTheSeas.webp";
+import iconImage from "./ParagonIcon.webp";
+
+export const ParagonImages = {
+  dart: dartImage,
+  ninja: ninjaImage,
+  tack: tackImage,
+  boomer: boomerImage,
+  ace: aceImage,
+  wizard: wizardImage,
+  engi: engiImage,
+  spike: spikeImage,
+  sub: subImage,
+  boat: boatImage,
+  icon: iconImage
+};
