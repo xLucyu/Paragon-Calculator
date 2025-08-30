823b30e9dc52102a3321d52faa60f61c7706958b
xLucyu <xlucyyu@gmail.com>
2025-08-30 10:11:49 +0200

    prepared output fields

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,2 +1,3 @@
 export type { Paragon } from "./paragon";
 export type { PowerFactors } from "./powerFactors";
+export type { Input } from "./input";
---
+++ src/interfaces/input.ts
@@ -0,0 +1,5 @@
+export interface Input {
+  key: string;
+  displayName: string;
+  max: number;
+}
